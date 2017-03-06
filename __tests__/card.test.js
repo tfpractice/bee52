cb03964123ea82e2d73b17527635a9aaeaff54e3
theory friction practice <jackson.brebnor@gmail.com>
2017-03-06 15:43:20 -0500

    setSuit setRank

--- __tests__/card.test.js
+++ __tests__/card.test.js
@@ -1,10 +1,29 @@
 import 'jasmine-expect';
-import { card, } from 'src/card';
+import { card,rank,setRank,setSuit,suit, } from 'src/card';
 describe('Card', () => {
   describe('card', () => {
     it('returns an object with a suit and rank', () => {
       console.log(card('2','CLUBS'));
       expect(card('2','CLUBS')).toBeObject();
+    });
+  });
+  describe('suit', () => {
+    it('returns the suit of the card', () => {
+      expect(suit({ suit: 'DIAMONDS', })).toEqual('DIAMONDS');  
+    });
+  });
+  describe('rank', () => {
+    it('returns the rank property', () => {
+      expect(rank({ rank: '2', })).toEqual('2');
+    });
+  });
+  describe('setRank', () => {
+    it('sets the rank of a card', () => {
+      expect(rank(setRank('3')(card('2','CLUBS')))).toEqual('3');
+    });
+  }); describe('setSuit', () => {
+    it('sets the suit of a card', () => {
+      expect(suit(setSuit('HEARTS')(card('2','CLUBS')))).toEqual('HEARTS');
     });
   });
 });
--- src/card.js
+++ src/card.js
@@ -1,1 +1,7 @@
-export const card = (rank, suit) => ({ rank, suit, });
+export const card = (rank, suit) => ({ rank, suit, toString: () => `${rank}_${suit}`, });
+
+export const suit = ({ suit, }) => suit;
+export const rank = ({ rank, }) => rank;
+
+export const setSuit = suit => c => card(rank(c), suit);
+export const setRank = rank => c => card(rank, suit(c));
