064bda9979cd5b2596521cbbab610ae9500a96b4
theory friction practice <jackson.brebnor@gmail.com>
2017-03-06 14:40:28 -0500

    basic ranks

--- __tests__/rank.test.js
+++ __tests__/rank.test.js
@@ -1,10 +1,48 @@
 import 'jasmine-expect';
-import { RANKS, } from 'src/rank';
+import { getNext,getPrev,idxMap,indexVal,modIdx,
+nextRank, pairId,prevRank, RANKS, VALUES, } from 'src/rank';
 
 describe('rank module', () => {
   describe('RANKS', () => {
     it('is an array of thirteen ranks', () => {
       expect(RANKS).toBeArray();
+    });
+  });
+  describe('VALUES', () => {
+    it('is an array of thirteen ranks', () => {
+      expect(VALUES instanceof Map).toBeTrue();
+      expect(VALUES.size).toEqual(13);
+    });
+  });
+  describe('pairId', () => {
+    it('return an array of KV pairs', () => {
+      console.log(pairId('a',1));
+      expect(pairId('a',1)).toEqual([ 'a',1, ]);
+    });
+  });
+  describe('modIdx', () => {
+    it('returns the element at the id mod array length', () => {
+      expect(modIdx([ 1,2,3, ])(4)).toEqual(2);
+    });
+  }); 
+  describe('getNext', () => {
+    it('returns the element at the id mod array length', () => {
+      expect(getNext([ 1,2,3, ])(3)).toEqual(1);
+    });
+  }); 
+  describe('getPrev', () => {
+    it('returns the element at the id mod array length', () => {
+      expect(getPrev([ 1,2,3, ])(3)).toEqual(2);
+    });
+  });
+  describe('nextRank', () => {
+    it('returns the next rank ', () => {
+      expect(nextRank('k')).toEqual('a');
+    });
+  });
+  describe('prevRank', () => {
+    it('returns the next rank ', () => {
+      expect(prevRank('k')).toEqual('q');
     });
   });
 });
--- src/rank.js
+++ src/rank.js
@@ -1,1 +1,11 @@
-export const RANKS = [ 'a',2,3,4,5,6,7,8,9,10,'j','q', 'k', ];
+export const RANKS = [ 'a','2','3','4','5','6','7','8','9','10','j','q', 'k', ];
+export const indexVal = (el, val) => [ el,val, ];
+export const pairId = (el, idx) => [ el,idx, ];
+export const idxMap = arr => new Map(arr.map(pairId));
+export const VALUES = idxMap(RANKS);
+export const modIdx = arr => id => arr[(id % arr.length)];
+export const getNext = arr => r => modIdx(arr)(idxMap(arr).get(r) + 1);
+export const getPrev = arr => r => modIdx(arr)(idxMap(arr).get(r) - 1);
+
+export const nextRank = r => getNext(RANKS)(r);
+export const prevRank = r => getPrev(RANKS)(r);
