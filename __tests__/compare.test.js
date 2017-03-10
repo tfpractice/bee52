fc5dbfc19c5cf77e3261609f98a8dd52c99d6319
theory friction practice <jackson.brebnor@gmail.com>
2017-03-10 14:54:52 -0500

    compares

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,5 +1,19 @@
-describe('description', () => {
-  it('description', () => {
-    expect(true).toBeTruthy();
+import 'jasmine-expect';
+import { card, sameRank, sameSuit, } from 'src/card';
+
+const AH = card('a', 'HEARTS');
+const AS = card('a', 'SPADES');
+const KS = card('K', 'SPADES');
+
+describe('compares', () => {
+  describe('sameRank', () => {
+    it('chekcs if two cards have same rank', () => {
+      expect(sameRank(AH)(AS)).toBeTrue();
+    });
+  });
+  describe('sameSuit', () => {
+    it('compares suites for equality', () => {
+      expect(sameSuit(AS)(KS)).toBeTrue();
+    });
   });
 });
