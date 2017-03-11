cd082f9b79db88cea3d52c4bd9bc98d7c8c5474d
theory friction practice <jackson.brebnor@gmail.com>
2017-03-10 19:32:03 -0500

    immutable shuffle

--- src/deck.js
+++ src/deck.js
@@ -7,12 +7,14 @@ export const genSuit = s => RANKS.map(r => card(r, s));
 
 export const deck = () => SUITS.map(genSuit).reduce(binFlat,[]);
 
-export const shuffRed = (shuff, el,id, a) => {
+export const shuffRed = (shuff, el,id, arr) => {
   const j = Math.floor(Math.random() * id);
+  const shuff2 = [ ...shuff, ];
 
-  [ shuff[id], shuff[j], ] = [ a[j], el, ];
-  return shuff;
+  [ shuff2[id], shuff2[j], ] = [ arr[j], el, ];
+  return shuff2;
 };
+
 export const shuffle = cards => cards.reduce(shuffRed, cards);
 export const draw = (amt = 0) => cards => cards.splice(0,amt);
 export const add = cards => hand => [ ...hand,...cards, ];
