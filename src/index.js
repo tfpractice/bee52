de70fca92a882e43c8abae3496c107e8965d44bd
theory friction practice <jackson.brebnor@gmail.com>
2017-03-12 21:17:39 -0400

    exports compare

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as Card from './card';
+import * as Compare from './compare';
 import * as Deck from './deck';
 import * as Rank from './rank';
 import * as Suit from './suit';
 
-export { Card,Deck,Rank,Suit, };
+export { Card,Compare,Deck,Rank,Suit, };
