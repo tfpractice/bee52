c5a7c811898323e3e854418f589098bbb6256b55
theory friction practice <jackson.brebnor@gmail.com>
2017-03-13 19:18:32 -0400

    spreads find op

--- src/deck/find.js
+++ src/deck/find.js
@@ -1,8 +1,8 @@
-import { spread, } from 'fenugreek-collections';
+import { some, spread, } from 'fenugreek-collections';
 import { isMatch, } from '../compare';
 
 export const idx = arr => c => spread(arr).findIndex(isMatch(c));
 export const find = arr => c => spread(arr).find(isMatch(c));
 
-export const contains = arr => c => arr.some(isMatch(c));
+export const contains = arr => c => some(arr)(isMatch(c));
 export const xContains = arr => c => !contains(arr)(c);
