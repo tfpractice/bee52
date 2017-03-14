82d8e6a0642ca27102db9e1e14f17e27c7cc51cd
theory friction practice <jackson.brebnor@gmail.com>
2017-03-13 20:11:19 -0400

    collections are now fluent

--- src/deck/draw.js
+++ src/deck/draw.js
@@ -1,4 +1,4 @@
-import { spread, } from 'fenugreek-collections';
+import { filter, spread, } from 'fenugreek-collections';
 import { xMatch, } from '../compare';
 import { find, idx, } from './find';
 import { diff, union, } from './dedupe';
@@ -6,7 +6,7 @@ import { diff, union, } from './dedupe';
 export const draw = amt => arr => spread(arr).splice(0, amt);
 export const drawTo = c => arr => find(arr)(c) ? draw(idx(arr)(c) + 1)(arr) : [];
 
-export const add = c => arr => arr.filter(xMatch(c)).concat(c);
+export const add = c => arr => filter(arr)(xMatch(c)).concat(c);
 export const addBin = (arr, c) => add(c)(arr);
 
 export const addCards = (...cards) => arr => union(cards)(arr);
--- src/deck/filter.js
+++ src/deck/filter.js
@@ -4,6 +4,7 @@ import { hasRank, hasSuit, } from '../card';
 
 export const byRank = rank => cards => filter(cards)(hasRank(rank));
 export const bySuit = suit => cards => filter(cards)(hasSuit(suit));
+
 export const byMatch = c => cards => filter(cards)(isMatch(c));
 export const exclude = c => cards => filter(cards)(xMatch(c));
 export const remove = exclude;
